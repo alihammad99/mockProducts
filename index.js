e304926dfbafc7efb5ad99c9ab1a1776358b5497
Ali Hammad <aliressan.ar@gmail.com>
2023-05-11 15:14:57 +0300

    Updates

--- index.js
+++ index.js
@@ -12,6 +12,27 @@ app.get("/", (req, res) => {
   res.send("Hi there");
 });
 
+app.get("/api/categories", (req, res) => {
+  try {
+    const { categories } = data;
+    res.status(200).json(categories);
+  } catch (e) {
+    res.status(404).send({ message: e.message });
+  }
+});
+app.get("/api/products-by-category", (req, res) => {
+  const { category } = req.body;
+  try {
+    const products = data.products[category];
+    if (!products) {
+      return res.status(404).send({ message: "Category does not Found" });
+    }
+    res.status(200).json(products);
+  } catch (e) {
+    res.status(404).send({ message: e.message });
+  }
+});
+
 app.get("/api/products", (req, res) => {
   try {
     const { products } = data;
@@ -21,7 +42,7 @@ app.get("/api/products", (req, res) => {
   }
 });
 app.get("/api/product", (req, res) => {
-  const { id, category, subCategory } = req;
+  const { id, category, subCategory } = req.body;
   try {
     const product = data.products[category][subCategory][id];
     res.status(200).json(product);
